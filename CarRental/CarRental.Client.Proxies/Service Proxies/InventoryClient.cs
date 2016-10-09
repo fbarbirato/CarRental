1859dd0c6f975c05b7507aefdf2707fb172bb1bb
FELIPE-PC\fbarb <fbarbirato@gmail.com>
2016-10-09 18:32:36 -0300

    Adding client proxies

---
+++ CarRental/CarRental.Client.Proxies/Service Proxies/InventoryClient.cs
@@ -0,0 +1,67 @@
+﻿using CarRental.Client.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using CarRental.Client.Entities;
+using System.ComponentModel.Composition;
+
+namespace CarRental.Client.Proxies.Service_Proxies
+{
+    [Export(typeof(IInventoryService))]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    public class InventoryClient : ClientBase<IInventoryService>, IInventoryService
+    {
+        public void DeleteCar(int carId)
+        {
+            Channel.DeleteCar(carId);
+        }
+
+        public Task DeleteCarAsync(int carId)
+        {
+            return Channel.DeleteCarAsync(carId);
+        }
+
+        public Car[] GetAllCars()
+        {
+            return Channel.GetAllCars();
+        }
+
+        public Task<Car[]> GetAllCarsAsync()
+        {
+            return Channel.GetAllCarsAsync();
+        }
+
+        public Car[] GetAvailableCars(DateTime pickupDate, DateTime returnDate)
+        {
+            return Channel.GetAvailableCars(pickupDate, returnDate);
+        }
+
+        public Task<Car[]> GetAvailableCarsAsync(DateTime pickupDate, DateTime returnDate)
+        {
+            return Channel.GetAvailableCarsAsync(pickupDate, returnDate);
+        }
+
+        public Car GetCar(int carId)
+        {
+            return Channel.GetCar(carId);
+        }
+
+        public Task<Car> GetCarAsync(int carId)
+        {
+            return Channel.GetCarAsync(carId);
+        }
+
+        public Car UpdateCar(Car car)
+        {
+            return Channel.UpdateCar(car);
+        }
+
+        public Task<Car> UpdateCarAsync(Car car)
+        {
+            return Channel.UpdateCarAsync(car);
+        }
+    }
+}
