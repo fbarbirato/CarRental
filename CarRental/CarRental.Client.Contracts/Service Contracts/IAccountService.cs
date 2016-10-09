707bfec25324bdfbdc51cc14c239cb5de3a635fe
FELIPE-PC\fbarb <fbarbirato@gmail.com>
2016-10-09 17:32:19 -0300

    Adding client contracts

---
+++ CarRental/CarRental.Client.Contracts/Service Contracts/IAccountService.cs
@@ -0,0 +1,34 @@
+﻿using CarRental.Client.Entities;
+using Core.Common.Contracts;
+using Core.Common.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Client.Contracts
+{
+    [ServiceContract]
+    public interface IAccountService : IServiceContract
+    {
+        [OperationContract]
+        [FaultContract(typeof(NotFoundException))]
+        [FaultContract(typeof(AuthorizationValidationException))]
+        Account GetCustomerAccountInfo(string loginEmail);
+
+        [OperationContract]
+        [FaultContract(typeof(AuthorizationValidationException))]
+        [TransactionFlow(TransactionFlowOption.Allowed)]
+        void UpdateCustomerAccountInfo(Account account);
+
+        #region Async operations
+
+        Task<Account> GetCustomerAccountInfoAsync(string loginEmail);
+
+        Task UpdateCustomerAccountInfoAsync(Account account);
+
+        #endregion
+    }
+}
---
+++ CarRental/CarRental.Client.Contracts/Service Contracts/IInventoryService.cs
@@ -0,0 +1,48 @@
+﻿using CarRental.Client.Entities;
+using Core.Common.Contracts;
+using Core.Common.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Client.Contracts
+{
+    [ServiceContract]
+    public interface IInventoryService : IServiceContract
+    {
+        [OperationContract]
+        [FaultContract(typeof(NotFoundException))]
+        Car GetCar(int carId);
+
+        [OperationContract]
+        Car[] GetAllCars();
+
+        [OperationContract]
+        [TransactionFlow(TransactionFlowOption.Allowed)]
+        Car UpdateCar(Car car);
+
+        [OperationContract]
+        [TransactionFlow(TransactionFlowOption.Allowed)]
+        void DeleteCar(int carId);
+
+        [OperationContract]
+        Car[] GetAvailableCars(DateTime pickupDate, DateTime returnDate);
+
+        #region Async operations
+
+        Task<Car> UpdateCarAsync(Car car);
+
+        Task DeleteCarAsync(int carId);
+
+        Task<Car> GetCarAsync(int carId);
+
+        Task<Car[]> GetAllCarsAsync();
+
+        Task<Car[]> GetAvailableCarsAsync(DateTime pickupDate, DateTime returnDate);
+
+        #endregion
+    }
+}
