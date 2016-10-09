8d3f957b008cf0109febcd071cdbdfb099083d1d
FELIPE-PC\fbarb <fbarbirato@gmail.com>
2016-10-09 15:22:25 -0300

    Adding unnatended service to cancel dead reservations.

--- CarRental/CarRental.ServiceHost.Console/Program.cs
+++ CarRental/CarRental.ServiceHost.Console/Program.cs
@@ -1,9 +1,14 @@
-﻿using CarRental.Business.Managers;
+﻿using CarRental.Business.Bootstrapper;
+using CarRental.Business.Managers;
+using Core.Common.Core;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Transactions;
 using SM = System.ServiceModel;
 
 namespace CarRental.ServiceHost
@@ -12,6 +17,11 @@ namespace CarRental.ServiceHost
     {
         static void Main(string[] args)
         {
+            var principal = new GenericPrincipal(new GenericIdentity("fbarb"), new string[] { "CarRentalAdmin" });
+            Thread.CurrentPrincipal = principal;
+
+            ObjectBase.Container = MEFLoader.Init();
+
             Console.WriteLine("Starting up services...");
             Console.WriteLine();
 
@@ -23,13 +33,54 @@ namespace CarRental.ServiceHost
             StartService(hostRentalManager, "RentalManager");
             StartService(hostAccountManager, "AccountManager");
 
+            var timer = new System.Timers.Timer(10000);
+            timer.Elapsed += OnTimerElapsed;
+            timer.Start();
+                        
+            Console.WriteLine("Reservation monitor started.");
+
             Console.WriteLine();
             Console.WriteLine("Press [Enter] to exit.");
             Console.ReadLine();
 
+            timer.Stop();
+
+            Console.WriteLine("Reservation monitor stopped.");
+
             StopService(hostInventoryManager, "InventoryManager");
             StopService(hostRentalManager, "RentalManager");
             StopService(hostAccountManager, "AccountManager");
+        }
+
+        private static void OnTimerElapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            CancelDeadReservations();
+        }
+
+        private static void CancelDeadReservations()
+        {
+            var rentalManager = new RentalManager();
+
+            var reservations = rentalManager.GetDeadReservations();
+
+            if (reservations != null)
+            {
+                foreach (var reservation in reservations)
+                {
+                    using (var scope = new TransactionScope())
+                    {
+                        try
+                        {
+                            rentalManager.CancelReservation(reservation.ReservationId);
+                            scope.Complete();
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("There was an exception then attempting to cancel reservation '{0}'.", reservation.ReservationId);
+                        }
+                    }
+                }
+            }
         }
 
         static void StartService(SM.ServiceHost host, string serviceDescription)
