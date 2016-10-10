2f04ce531ce78424651b8292408b6a7758503acd
FELIPE-PC\fbarb <fbarbirato@gmail.com>
2016-10-10 18:55:20 -0300

    Adding tests for proxies and DI

---
+++ CarRental/CarRental.Client.Proxies.Tests/ProxyObtainmentTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Core.Common.Core;
+using CarRental.Client.Bootstrapper;
+using CarRental.Client.Contracts;
+using CarRental.Client.Proxies.Service_Proxies;
+using Core.Common.Contracts;
+
+namespace CarRental.Client.Proxies.Tests
+{
+    [TestClass]
+    public class ProxyObtainmentTests
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            ObjectBase.Container = MEFLoader.Init();
+        }
+
+        [TestMethod]
+        public void obtain_proxy_from_container_using_service_contract()
+        {
+            var proxy = ObjectBase.Container.GetExportedValue<IInventoryService>();
+
+            Assert.IsTrue(proxy is InventoryClient);
+        }
+
+        [TestMethod]
+        public void obtain_proxy_from_service_factory()
+        {
+            var factory = new ServiceFactory();
+
+            var proxy = factory.CreateClient<IInventoryService>();
+
+            Assert.IsTrue(proxy is InventoryClient);
+        }
+
+        [TestMethod]
+        public void obtain_service_factory_and_proxy_from_container()
+        {
+            var factory = ObjectBase.Container.GetExportedValue<IServiceFactory>();
+
+            var proxy = factory.CreateClient<IInventoryService>();
+
+            Assert.IsTrue(proxy is InventoryClient);
+        }
+    }
+}
---
+++ CarRental/CarRental.Client.Proxies.Tests/ServiceAccessTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CarRental.Client.Proxies.Service_Proxies;
+
+namespace CarRental.Client.Proxies.Tests
+{
+    [TestClass]
+    public class ServiceAccessTests
+    {
+        [TestMethod]
+        public void test_inventory_client_connection()
+        {
+            var proxy = new InventoryClient();
+
+            proxy.Open();
+        }
+
+        [TestMethod]
+        public void test_account_client_connection()
+        {
+            var proxy = new AccountClient();
+
+            proxy.Open();
+        }
+
+        [TestMethod]
+        public void test_rental_client_connection()
+        {
+            var proxy = new RentalClient();
+
+            proxy.Open();
+        }
+    }
+}
